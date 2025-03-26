8d683e3bd7541fdc116c0ab7924d4adfb843d104
ndjo-byte <jonesn.96@gmail.com>
2025-03-26 16:40:20 +0100

    model relationships deployed and visible on pgadmin

--- database.py
+++ database.py
@@ -1,7 +1,21 @@
 import os
-from sqlmodel import create_engine
+from sqlmodel import SQLModel, create_engine
 from dotenv import load_dotenv
+from models import Supplier, Equipment, FailureIncident
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set. Check your .env file.")
+
+print(f"Connecting to: {DATABASE_URL}")
+
+def create_tables() -> None:
+    SQLModel.metadata.create_all(engine)
+
+if __name__ == "__main__": 
+    create_tables()
+    print("Database and Tables Created")
+    print(SQLModel.metadata.tables.keys())
--- models.py
+++ models.py
@@ -1,22 +1,28 @@
 from sqlmodel import SQLModel, Field, Relationship, create_engine
 from typing import Optional, List
-from sqlalchemy import Date
+from datetime import date
 
 
-
-class Suppliers(SQLModel, Table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+class Supplier(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     name: str
     country: str
 
-class Equipment(SQLModel, Table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    supplier_id : Optional[int]  = Field(default=None, foreign_key="Suppliers.id")
-    game_type: str
-    installation_date: Date
+    equipment: List["Equipment"] = Relationship(back_populates="supplier")
 
-class FailureIncidents(SQLModel, Table=True):
+class Equipment(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    failed_equipmente_id: Optional[int] = Field(default=None, foreign_key="Equipment.id")
+    supplier_id : Optional[int]  = Field(default=None, foreign_key="supplier.id")
+    game_type: str
+    installation_date: date
+
+    supplier: Supplier = Relationship(back_populates="equipment")
+    failure: List["FailureIncident"] = Relationship(back_populates="equipment")
+
+class FailureIncident(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    failed_equipmente_id: Optional[int] = Field(default=None, foreign_key="equipment.id")
     failure_type: str
-    failure_date: Date
+    failure_date: date
+
+    equipment: Equipment = Relationship(back_populates="failure")
